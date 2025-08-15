6c9436423d0d0c8b14a3d8f6fd0ced2906eb87ed
Daniel Garado <122231506+ddgarado@users.noreply.github.com>
2025-08-15 17:31:11 +0800

    main menu
    Adjusted main menu ui

--- Assets/_Scripts/Main_Menu_Scripts/SceneLoader.cs
+++ Assets/_Scripts/Main_Menu_Scripts/SceneLoader.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuHandler : MonoBehaviour
+
+public class SceneLoader : MonoBehaviour
 {
 
-    public void LoadTrashScannerScene()
+    public void LoadScanScene()
     {
         SceneManager.LoadScene("AIScannerScene");
     }
 
-    public void LoadCardCollectionScene()
+    public void LoadCardsScene()
     {
         SceneManager.LoadScene("CardCollectionScene");
     }
